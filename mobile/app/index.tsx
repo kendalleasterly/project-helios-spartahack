a596231760858437f1d8e4a7de77809176be0088
breissic <breissic@users.noreply.github.com>
2026-01-31 14:27:14 -0500

    Added camera component and 1 FPS still frame capture
    next step -> initialize connection with backend laptop and send stills
    to backend. also start tracking sensors (accelerometer, etc)

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,10 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import CameraView from "../components/CameraView";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1 }}>
+      <CameraView />
     </View>
   );
 }

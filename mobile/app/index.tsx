e77a537a36dfeb4ccfe30672aa43e730e00db775
BenGottschall <bgottschall89@gmail.com>
2026-01-31 18:29:38 -0500

    Replace scaffold with mobile app from mobile-pipeline

---
+++ mobile/app/index.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import CameraView from "../components/CameraView";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1 }}>
+      <CameraView />
+    </View>
+  );
+}
